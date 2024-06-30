5ee73dc0d6f582f76617c3662ba8f1c182b07b44
maltin1 <r0mh7q@gmail.com>
2024-06-30 12:40:57 -0500

    funcion draw

--- DisjointSets.py
+++ DisjointSets.py
@@ -1,5 +1,5 @@
-
-
+import graphviz as gv
+'''Nota: Por alguna razon el codigo no corre en visual pero si en colab :c'''
 
 #n = nodos
 # esto funciona para grafos sin pesos :)
@@ -25,11 +25,25 @@ class Disjoint:
             self.parent[a_root] += self.parent[b_root]
             self.parent[b_root] = a_root
 
+        else:
+            self.parent[b_root] += self.parent[a_root]
+            self.parent[a_root] = b_root
+
+    def draw(self):
+        dot = gv.Digraph('parent')
+        dot.graph_attr['nose'] = 'BT'
+        for i in range(len(self.parent)):
+            dot.node(str(i))
+        for i, e in enumerate(self.parent):
+            if e >= 0:
+                dot.edge(str(e), str(i))
+        return dot
+
+
 #un grafo con 6 nodos 
 graph = Disjoint(6)
 graph.union(0,1)
 graph.union(1,2)
-graph.union(3,4)
+graph.union(2,3)
 graph.union(4,5)
-
-graph.find(4)
+graph.find(3)
