fa30b6d518c774430db80c3f413b98e391e3f3e4
maltin1 <r0mh7q@gmail.com>
2024-07-01 15:27:31 -0500

    reading txt

--- pregunta_tipo_final.py
+++ pregunta_tipo_final.py
@@ -1,30 +1,28 @@
 
-#leer el txt
-def read(file):
-    with open(file, 'r') as file:
-        lines = file.readline
+'''
+- la primera linea es T
+- las lineas con 3 numeros son un grafo (x,y,z) 
+x=nodoinicio, y = nododestino z = peso. 
+- en la linea siguiente de una con 3 elementos se almacena el valor de K 
+'''
 
-    index = 0
-    T = int(lines[index])
-    index += 1
-    cases = []
+#leer el txt
+def read_txt(file):
+    with open(file, 'r') as file:
+        lines = file.readlines()
+
+    T = int(lines[0].strip()) #cantidad de casos
+    index = 1
+    grafos = []
+    #linea 0 -> T
+    #linea 1 -> N
+    #linea 2... -> (x,y,z)
 
     for _ in range(T):
-        N = int(lines[index])
-        index += 1
-        zones = []
+        #N = nodos / index = 1
+        N = int(lines[index]) #se le asigna al entero de linea 1
+        index +=1 #leer linea 2 en adelante
+        zonas_peligrosas = []
+
         for _ in range(N):
-            x,y,z = map(int, lines[index].split())
-            zones.append((x,y,z))
-            index += 1
-        k = int(lines[index])
-        index += 1
-        cases.append((zones,k))
-
-    return cases
-
-
-
-
-info = "linternas.txt"
-read(info)
+            x, y, z = map(int, lines[index])
